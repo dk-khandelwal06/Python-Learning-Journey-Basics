530910edc1e04c8678f73f0c2d4570c81bd9589d
Daksh Khandelwal <dk.khandelwaliitj@gmail.com>
2026-08-19 15:21:40 +0530

    Add Chapter 13 Files

---
+++ CHAPTER 13/03_join.py
@@ -0,0 +1,4 @@
+a = ["Daksh", "Rohan", "Shubham"]
+
+final = "::".join(a)
+print(final)
---
+++ CHAPTER 13/04_format.py
@@ -0,0 +1,5 @@
+a = "{1} is a good {0}".format("Daksh", "boy")
+b = "{} is a good {}".format("Daksh", "boy") # By default 0,1,2....n
+
+print(a)
+print(b)
